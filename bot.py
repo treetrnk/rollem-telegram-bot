6a763ff1ccaf5d4c832f7de2fc28f71aecb25197
Nathan Hare <nhare330@gmail.com>
2021-03-14 07:02:48 -0400

    Remove unused code

--- bot.py
+++ bot.py
@@ -206,28 +206,6 @@ logger = logging.basicConfig(
         handlers = [stream_handler,file_handler],
         level=logging.DEBUG,
     )
-#logger = logging.getLogger()
-#logger.setLevel(logging.INFO)
-#stream_handler = logging.StreamHandler(sys.stdout)
-#stream_handler.setLevel(logging.DEBUG)
-#logger.addHandler(stream_handler)
-#file_handler = logging.FileHandler('roll.log')
-#file_handler.setLevel(logging.DEBUG)
-#logger.addHandler(file_handler)
-#formatter = logging.Formatter('====> %(asctime)s | %(name)s | %(levelname)s | %(message)s')
-#logger.setFormatter(formatter)
-
-#file_logger = logging.basicConfig(
-#        filename='roll.log', 
-#        encoding='utf-8', 
-#        format='====> %(asctime)s | %(name)s | %(levelname)s | %(message)s',
-#        level=logging.INFO
-#    )
-#stdout_logger = logging.basicConfig(
-#        format='====> %(asctime)s | %(name)s | %(levelname)s | %(message)s', 
-#        stream=sys.stdout,
-#        level=logging.DEBUG
-#    )
 
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
